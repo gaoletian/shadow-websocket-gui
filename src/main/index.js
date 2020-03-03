c11f8af4b0474798b62e1aa558a361e571d22ea1
高乐天 <67890211@qq.com>
2020-03-03 17:39:26 +0800

    fix: use default applicationMenu(file|edit|...)

--- src/main/index.js
+++ src/main/index.js
@@ -40,20 +40,11 @@ function setApplicationMenu() {
 		// :::
 		Menu.setApplicationMenu(
 			Menu.buildFromTemplate([
-				{
-					label: 'Edit',
-					submenu: [
-						{ role: 'undo' },
-						{ role: 'redo' },
-						{ type: 'separator' },
-						{ role: 'cut' },
-						{ role: 'copy' },
-						{ role: 'paste' },
-						{ role: 'pasteandmatchstyle' },
-						{ role: 'delete' },
-						{ role: 'selectall' }
-					]
-				}
+				{ role: 'appMenu' },
+				{ role: 'fileMenu' },
+				{ role: 'editMenu' },
+				{ role: 'viewMenu' },
+				{ role: 'windowMenu' }
 			])
 		);
 	}
