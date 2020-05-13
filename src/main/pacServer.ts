2f7e0279fb62bad1a3dd23c1e9eb00941906e9c3
gaoletian <gaolet@chanjet.com>
2020-05-13 21:38:07 +0800

    fix: proxy.pac update by localPort change

--- src/main/pacServer.ts
+++ src/main/pacServer.ts
@@ -1,17 +1,27 @@
 import * as http from 'http';
 import * as fs from 'fs';
 import { PAC_PATH } from '../share';
-import { setupSystemProxy, loadSetting, getAutoConfigUrl } from './utils';
+import {
+  setupSystemProxy,
+  loadSetting,
+  getAutoConfigUrl,
+  loadConfig
+} from './utils';
 
 export class PacServer {
   server = null;
   startup() {
     const { pacPort } = loadSetting();
+    const { localPort } = loadConfig();
     this.server = http
       .createServer((req, res) => {
         let pac = fs.readFileSync(PAC_PATH, {
           encoding: 'utf8'
         });
+        pac = pac.replace(
+          /var proxy = ".*"/g,
+          `var proxy = "SOCKS5 127.0.0.1:${localPort}; SOCKS 127.0.0.1:${localPort}; DIRECT;"`
+        );
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end(pac);
